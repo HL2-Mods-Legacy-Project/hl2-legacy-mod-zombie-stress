6a42a7a42457b718c2ad0049f3c056df4c67935b
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:06:07 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Zombie_Stress_1.1" `
+  -ModName "Zombie Stress 1.1" `
+  -ModFolder "Zombie Stress" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/zombie-stress/Zombie-Stress-1.1-Original.zip" `
+  -ModBaseFilesUrlHash "11affead772f2f4e7307889dcd7056911e07fd61f5c2eb0248ed2bd8820f2f75" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
